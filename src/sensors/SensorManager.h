0f3aeb2c9f9b50efb85699a998bf26f8d361643d
Topaz_Blue <90056887+Earnhbry000@users.noreply.github.com>
2025-01-09 19:32:10 -0500

    Add files via upload

---
+++ src/sensors/SensorManager.h
@@ -0,0 +1,133 @@
+/*
+	SlimeVR Code is placed under the MIT license
+	Copyright (c) 2022 TheDevMinerTV
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+#ifndef SLIMEVR_SENSORMANAGER
+#define SLIMEVR_SENSORMANAGER
+
+#include <i2cscan.h>
+
+#include <memory>
+
+#include "EmptySensor.h"
+#include "ErroneousSensor.h"
+#include "globals.h"
+#include "logging/Logger.h"
+#include "sensor.h"
+
+namespace SlimeVR {
+namespace Sensors {
+class SensorManager {
+public:
+	SensorManager()
+		: m_Logger(SlimeVR::Logging::Logger("SensorManager")) {}
+	void setup();
+	void postSetup();
+
+	void update();
+
+	std::vector<std::unique_ptr<Sensor>>& getSensors() { return m_Sensors; };
+	ImuID getSensorType(size_t id) {
+		if (id < m_Sensors.size()) {
+			return m_Sensors[id]->getSensorType();
+		}
+		return ImuID::Unknown;
+	}
+
+	uint8_t getActiveSensorCount() { return m_ActiveSensorCount; }
+
+private:
+	SlimeVR::Logging::Logger m_Logger;
+
+	std::vector<std::unique_ptr<Sensor>> m_Sensors;
+	uint8_t m_ActiveSensorCount = 0;
+
+	template <typename ImuType>
+	std::unique_ptr<Sensor> buildSensor(
+		uint8_t sensorID,
+		uint8_t addrSuppl,
+		float rotation,
+		uint8_t sclPin,
+		uint8_t sdaPin,
+		bool optional = false,
+		int extraParam = 0
+	) {
+		const uint8_t address = ImuType::Address + addrSuppl;
+		m_Logger.trace(
+			"Building IMU with: id=%d,\n\
+                                address=0x%02X, rotation=%f,\n\
+                                sclPin=%d, sdaPin=%d, extraParam=%d, optional=%d",
+			sensorID,
+			address,
+			rotation,
+			sclPin,
+			sdaPin,
+			extraParam,
+			optional
+		);
+
+		// Now start detecting and building the IMU
+		std::unique_ptr<Sensor> sensor;
+
+		// Clear and reset I2C bus for each sensor upon startup
+		I2CSCAN::clearBus(sdaPin, sclPin);
+		swapI2C(sclPin, sdaPin);
+
+		if (I2CSCAN::hasDevOnBus(address)) {
+			m_Logger.info("Sensor %d found at address 0x%02X", sensorID + 1, address);
+		} else {
+			if (!optional) {
+				m_Logger.info("Mandatory sensor %d not found at address 0x%02X, attempting setup anyways...", sensorID + 1, address);
+			} else {
+				#ifdef SECOND_IMU
+				m_Logger.info("Optional sensor %d not found at address 0x%02X, attempting setup anyways...", sensorID + 1, address);
+				#else
+				sensor = std::make_unique<EmptySensor>(sensorID);
+				return sensor;
+				#endif
+			}
+		}
+
+		uint8_t intPin = extraParam;
+		sensor = std::make_unique<ImuType>(
+			sensorID,
+			addrSuppl,
+			rotation,
+			sclPin,
+			sdaPin,
+			intPin
+		);
+
+		sensor->motionSetup();
+		return sensor;
+	}
+	uint8_t activeSCL = 0;
+	uint8_t activeSDA = 0;
+	bool running = false;
+	void swapI2C(uint8_t scl, uint8_t sda);
+
+	uint32_t m_LastBundleSentAtMicros = micros();
+};
+}  // namespace Sensors
+}  // namespace SlimeVR
+
+#endif  // SLIMEVR_SENSORFACTORY_H_
---
+++ src/sensors/softfusion/drivers/icm42688.h
@@ -0,0 +1,213 @@
+/*
+	SlimeVR Code is placed under the MIT license
+	Copyright (c) 2024 Tailsy13 & SlimeVR Contributors
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <cstdint>
+
+namespace SlimeVR::Sensors::SoftFusion::Drivers {
+
+// Driver uses acceleration range at 4g
+// and gyroscope range at 1000dps
+// Gyroscope ODR = 500Hz, accel ODR = 100Hz
+// Timestamps reading not used, as they're useless (constant predefined increment)
+
+template <typename I2CImpl>
+struct ICM42688 {
+	static constexpr uint8_t Address = 0x68;
+	static constexpr auto Name = "ICM-42688";
+	static constexpr auto Type = ImuID::ICM42688;
+
+	static constexpr float GyrTs = 1.0 / 500.0;
+	static constexpr float AccTs = 1.0 / 100.0;
+	static constexpr float TempTs = 1.0 / 500.0;
+
+	static constexpr float MagTs = 1.0 / 100;
+
+	static constexpr float GyroSensitivity = 32.8f;
+	static constexpr float AccelSensitivity = 8192.0f;
+
+	static constexpr bool Uses32BitSensorData = true;
+
+	static constexpr float TemperatureBias = 25.0f;
+	static constexpr float TemperatureSensitivity = 2.07f;
+
+	// Temperature stability constant - how many degrees of temperature for the bias to
+	// change by 0.01 Though I don't know if it should be 0.1 or 0.01, this is a guess
+	// and seems to work better than 0.1
+	static constexpr float TemperatureZROChange = 20.0f;
+
+	// VQF parameters
+	// biasSigmaInit and and restThGyr should be the sensor's typical gyro bias
+	// biasClip should be 2x the sensor's typical gyro bias
+	// restThAcc should be the sensor's typical acceleration bias
+	static constexpr VQFParams SensorVQFParams{
+		.motionBiasEstEnabled = true,
+		.biasSigmaInit = 0.5f,
+		.biasClip = 1.0f,
+		.restThGyr = 0.5f,
+		.restThAcc = 0.196f,
+	};
+
+	I2CImpl i2c;
+	SlimeVR::Logging::Logger& logger;
+	ICM42688(I2CImpl i2c, SlimeVR::Logging::Logger& logger)
+		: i2c(i2c)
+		, logger(logger) {}
+
+	struct Regs {
+		struct WhoAmI {
+			static constexpr uint8_t reg = 0x75;
+			static constexpr uint8_t value = 0x47;
+		};
+		static constexpr uint8_t TempData = 0x1d;
+
+		struct DeviceConfig {
+			static constexpr uint8_t reg = 0x11;
+			static constexpr uint8_t valueSwReset = 1;
+		};
+		struct IntfConfig0 {
+			static constexpr uint8_t reg = 0x4c;
+			static constexpr uint8_t value
+				= (0 << 4) | (0 << 5)
+				| (0 << 6);  // fifo count in LE, sensor data in LE, fifo size in bytes
+		};
+		struct FifoConfig0 {
+			static constexpr uint8_t reg = 0x16;
+			static constexpr uint8_t value = (0b01 << 6);  // stream to FIFO mode
+		};
+		struct FifoConfig1 {
+			static constexpr uint8_t reg = 0x5f;
+			static constexpr uint8_t value
+				= 0b1 | (0b1 << 1) | (0b0 << 2)
+				| (0b0 << 4);  // fifo accel en=1, gyro=1, temp=0, hires=1
+		};
+		struct GyroConfig {
+			static constexpr uint8_t reg = 0x4f;
+			static constexpr uint8_t value
+				= (0b001 << 5) | 0b1111;  // 1000dps, odr=500Hz
+		};
+		struct AccelConfig {
+			static constexpr uint8_t reg = 0x50;
+			static constexpr uint8_t value = (0b010 << 5) | 0b0111;  // 4g, odr = 200Hz
+		};
+		struct PwrMgmt {
+			static constexpr uint8_t reg = 0x4e;
+			static constexpr uint8_t value
+				= 0b11 | (0b11 << 2);  // accel in low noise mode, gyro in low noise
+		};
+
+		// TODO: might be worth checking
+		// GYRO_CONFIG1
+		// GYRO_ACCEL_CONFIG0
+		// ACCEL_CONFIG1
+
+		static constexpr uint8_t FifoCount = 0x2e;
+		static constexpr uint8_t FifoData = 0x30;
+	};
+
+#pragma pack(push, 1)
+	struct FifoEntryAligned {
+		union {
+			struct {
+				int16_t accel[3];
+				int16_t gyro[3];
+				uint16_t temp;
+				uint16_t timestamp;
+				uint8_t xlsb;
+				uint8_t ylsb;
+				uint8_t zlsb;
+			} part;
+			uint8_t raw[19];
+		};
+	};
+#pragma pack(pop)
+
+	static constexpr size_t FullFifoEntrySize = sizeof(FifoEntryAligned) + 1;
+
+	bool initialize() {
+		// perform initialization step
+		i2c.writeReg(Regs::DeviceConfig::reg, Regs::DeviceConfig::valueSwReset);
+		delay(20);
+
+		i2c.writeReg(Regs::IntfConfig0::reg, Regs::IntfConfig0::value);
+		i2c.writeReg(Regs::GyroConfig::reg, Regs::GyroConfig::value);
+		i2c.writeReg(Regs::AccelConfig::reg, Regs::AccelConfig::value);
+		i2c.writeReg(Regs::FifoConfig0::reg, Regs::FifoConfig0::value);
+		i2c.writeReg(Regs::FifoConfig1::reg, Regs::FifoConfig1::value);
+		i2c.writeReg(Regs::PwrMgmt::reg, Regs::PwrMgmt::value);
+		delay(1);
+
+		return true;
+	}
+
+	template <typename AccelCall, typename GyroCall, typename TemperatureCall>
+	void bulkRead(
+		AccelCall&& processAccelSample,
+		GyroCall&& processGyroSample,
+		TemperatureCall&& processTemperatureSample
+	) {
+		const auto fifo_bytes = i2c.readReg16(Regs::FifoCount);
+
+		std::array<uint8_t, FullFifoEntrySize * 8> read_buffer;  // max 8 readings
+		const auto bytes_to_read = std::min(
+									   static_cast<size_t>(read_buffer.size()),
+									   static_cast<size_t>(fifo_bytes)
+								   )
+								 / FullFifoEntrySize * FullFifoEntrySize;
+		i2c.readBytes(Regs::FifoData, bytes_to_read, read_buffer.data());
+		for (auto i = 0u; i < bytes_to_read; i += FullFifoEntrySize) {
+			FifoEntryAligned entry;
+			memcpy(
+				entry.raw,
+				&read_buffer[i + 0x1],
+				sizeof(FifoEntryAligned)
+			);  // skip fifo header
+
+			const int32_t gyroData[3]{
+				static_cast<int32_t>(entry.part.gyro[0]) << 4 | (entry.part.xlsb & 0xf),
+				static_cast<int32_t>(entry.part.gyro[1]) << 4 | (entry.part.ylsb & 0xf),
+				static_cast<int32_t>(entry.part.gyro[2]) << 4 | (entry.part.zlsb & 0xf),
+			};
+			processGyroSample(gyroData, GyrTs);
+
+			if (entry.part.accel[0] != -32768) {
+				const int32_t accelData[3]{
+					static_cast<int32_t>(entry.part.accel[0]) << 4
+						| (static_cast<int32_t>(entry.part.xlsb) & 0xf0 >> 4),
+					static_cast<int32_t>(entry.part.accel[1]) << 4
+						| (static_cast<int32_t>(entry.part.ylsb) & 0xf0 >> 4),
+					static_cast<int32_t>(entry.part.accel[2]) << 4
+						| (static_cast<int32_t>(entry.part.zlsb) & 0xf0 >> 4),
+				};
+				processAccelSample(accelData, AccTs);
+			}
+
+			processTemperatureSample(static_cast<int16_t>(entry.part.temp), TempTs);
+		}
+	}
+};
+
+}  // namespace SlimeVR::Sensors::SoftFusion::Drivers
