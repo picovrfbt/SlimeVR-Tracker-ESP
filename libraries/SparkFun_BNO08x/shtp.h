9758826c130bafc09be9e0fbd1ee1f562fd1244e
Topaz_Blue <90056887+Earnhbry000@users.noreply.github.com>
2025-04-01 17:59:36 -0400

    Delete libraries/SparkFun_BNO08x directory

--- libraries/SparkFun_BNO08x/shtp.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include <Wire.h>
-#include <SPI.h>
-
-bool shtp_init(TwoWire *i2cBus, uint8_t address, uint8_t intPin);
-bool shtp_init_spi(SPIClass *spiBus, uint8_t csPin, uint8_t intPin, uint32_t spiSpeed);
